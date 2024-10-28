429bcef46d93045a42777b9a953ff6cdce9a6057
Ryan Kan <25820201+PhotonicGluon@users.noreply.github.com>
2024-10-28 16:56:47 +0800

    Add basic hasse layout

--- manim_utils/hasse/hasse_graph.py
+++ manim_utils/hasse/hasse_graph.py
@@ -1,7 +1,10 @@
-from typing import Hashable, List, Tuple
+import random
+from typing import Any, Hashable, List, Tuple, Union
 
-from manim.constants import DOWN, UP
+import networkx as nx
+import numpy as np
 from manim.mobject.graph import DiGraph
+from manim.typing import Point3D
 
 from manim_utils.hasse.augmented_dag import DAG
 
@@ -11,24 +14,25 @@ class HasseGraph(DiGraph):
     Hasse graph.
     """
 
-    def __init__(
-        self, vertices: List[Hashable], edges: List[Tuple[Hashable, Hashable]], level_buff: float = 0.5, **kwargs
-    ):
-        super().__init__(vertices, edges, **kwargs)
-
+    def __init__(self, vertices: List[Hashable], edges: List[Tuple[Hashable, Hashable]], **kwargs):
         # Generate the internal DAG data
         self._dag = DAG(vertices, edges)
 
-        # Other attributes
-        self._level_buff = level_buff
-
-        # Preprocess
-        # self._arrange_vertices()
+        # Generate the Digraph
+        super().__init__(vertices, edges, layout=self._hasse_layout, **kwargs)
 
     # Helper methods
-    def _arrange_vertices(self):
-        # Compute level 0 offset from the center
-        level_0_offset = self._dag.height / 2 * self._level_buff * DOWN
+    def _hasse_layout(
+        self, graph: nx.Graph, level_buff: float = 2, *args: Any, **kwargs: Any
+    ) -> dict[Hashable, Point3D]:
+        # TODO: Update layout
 
-        for vertex, mobject in self.vertices.items():
-            mobject.shift(level_0_offset + self._dag.get_level(vertex) * self._level_buff * UP)
+        dag_height = self._dag.height
+
+        layout_dict = {}
+        for i, node in enumerate(graph):
+            layout_dict[node] = np.array(
+                [i - len(graph) / 2 + 2 * random.random(), level_buff * (self._dag.get_level(node) - dag_height / 2), 0]
+            )
+        print(layout_dict)
+        return layout_dict
