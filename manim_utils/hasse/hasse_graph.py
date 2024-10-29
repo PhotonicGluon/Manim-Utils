00ac39ccb8e5d07465e0b551fcc097c4c727e3b6
PhotonicGluon <25820201+PhotonicGluon@users.noreply.github.com>
2024-10-29 17:24:25 +0800

    Implement Fruchterman-Reingold force-directed algorithm

--- manim_utils/hasse/hasse_graph.py
+++ manim_utils/hasse/hasse_graph.py
@@ -1,5 +1,5 @@
 from collections import defaultdict
-from typing import Hashable, List, Tuple
+from typing import Hashable, List, Literal, Tuple
 
 import networkx as nx
 import numpy as np
@@ -24,6 +24,7 @@ class HasseGraph(Graph):
         intra_level_spacing: float = 1.0,
         inter_level_spacing: float = 1.0,
         centering: bool = True,
+        seed: int = 10,
         **kwargs
     ):
         # Generate the internal DAG data
@@ -33,6 +34,7 @@ class HasseGraph(Graph):
         self._intra_level_spacing = intra_level_spacing
         self._inter_level_spacing = inter_level_spacing
         self._centering = centering
+        self._seed = seed
 
         # Generate the graph
         super().__init__(
@@ -48,45 +50,111 @@ class HasseGraph(Graph):
 
     # Helper methods
     def _hasse_layout(self, graph: nx.Graph, *args, **kwargs) -> dict[Hashable, Point3D]:
-        # TODO: Update layout
+        # TODO: Add docs
 
         # First get data from the computed DAG
         dag_height = self._dag.height
         dag_level_sizes = self._dag.level_sizes
+        num_nodes = self._dag.num_vertices
 
         # Then start laying out the nodes properly
         levels_node_counts = defaultdict(lambda: 1)
-        layout_dict = {}
-        for node in graph:
+        pos = np.empty((num_nodes, 2))
+        rand = np.random.RandomState(self._seed)
+        for i, node in enumerate(graph):
             # Get the node's level
             node_level = self._dag.get_level(node)
 
             # Determine the node's position
             x = self._intra_level_spacing * (levels_node_counts[node_level] - dag_level_sizes[node_level] / 2)
+            x += 0.9 * rand.rand() + 0.1  # Add randomness in the uniform interval [0.1, 1.0)
+            # TODO: Enforce that all nodes that came before indeed come before
+
             y = self._inter_level_spacing * (node_level - dag_height / 2)
 
             # Note it down in the layout dictionary
-            layout_dict[node] = np.array([x, y, 0])
+            pos[i] = x, y
 
             # Update
             levels_node_counts[node_level] += 1
 
-        # TODO: Use Fruchterman-Reingold force-directed algorithm
-        print(layout_dict)  # TODO: Remove
+        # Use a modified Fruchterman-Reingold force-directed algorithm to nicely arrange nodes along each level
+        pos = self._fruchterman_reingold(nx.to_numpy_array(graph, weight="weight"), pos)
 
         # Adjust to be centred
         if self._centering:
             # Get the range of x and y
-            x_range = [np.inf, -np.inf]
-            y_range = [np.inf, -np.inf]
-            for pos in layout_dict.values():
-                x_range = [min(x_range[0], pos[0]), max(x_range[1], pos[0])]
-                y_range = [min(y_range[0], pos[1]), max(y_range[1], pos[1])]
+            x_max, y_max = np.max(pos, axis=0)
+            x_min, y_min = np.min(pos, axis=0)
 
             # Center all the nodes
-            for node in layout_dict:
-                layout_dict[node][0] -= (x_range[0] + x_range[1]) / 2
-                layout_dict[node][1] -= (y_range[0] + y_range[1]) / 2
+            pos -= np.array([(x_max + x_min) / 2, (y_max + y_min) / 2])
 
-            print(layout_dict)  # TODO: Remove
+        # Generate final layout dictionary
+        layout_dict = {}
+        for i, node in enumerate(graph):
+            layout_dict[node] = np.array([*pos[i], 0])
+
+        # print(layout_dict)
         return layout_dict
+
+    @staticmethod
+    def _fruchterman_reingold(
+        A: np.ndarray,
+        pos: np.ndarray,
+        min_dist: float = 1.5,
+        temperature: float = 0.05,
+        freeze: Literal["x", "y"] = "y",
+        iterations: int = 50,
+    ):
+        # TODO: Add docs
+        # See https://networkx.org/documentation/stable/_modules/networkx/drawing/layout.html#spring_layout
+
+        # Get the number of nodes from our adjacency matrix
+        num_nodes, _ = A.shape
+
+        # Make sure positions are of same type as matrix
+        pos = pos.astype(A.dtype)
+
+        # Optimal distance between nodes
+        optimal_dist = np.sqrt(1.0 / num_nodes)
+
+        # Calculate initial temperature based on domain size
+        t = max(max(pos.T[0]) - min(pos.T[0]), max(pos.T[1]) - min(pos.T[1])) * temperature
+
+        # Simple cooling scheme: linearly step down by `dt` on each iteration so last iteration is size `dt`
+        dt = t / (iterations + 1)
+
+        # Perform Fruchterman-Reingold (fast)
+        delta = np.zeros((pos.shape[0], pos.shape[0], pos.shape[1]), dtype=A.dtype)
+        for i in range(iterations):
+            # Matrix of difference between points
+            delta = pos[:, np.newaxis, :] - pos[np.newaxis, :, :]
+
+            # Distance between points
+            distance = np.linalg.norm(delta, axis=-1)
+            np.clip(distance, min_dist, None, out=distance)  # Enforce minimum distance
+
+            # Displacement "force"
+            displacement = np.einsum(
+                "ijk,ij->ik", delta, (optimal_dist * optimal_dist / distance**2 - A * distance / optimal_dist)
+            )
+
+            # Calculate initial delta for positions
+            length = np.linalg.norm(displacement, axis=-1)
+            length = np.where(length < min_dist, temperature, length)
+            delta_pos = np.einsum("ij,i->ij", displacement, t / length)
+
+            # If freezing specified, freeze x or y
+            if freeze == "x":
+                freeze_index = 0
+            else:
+                freeze_index = 1
+            delta_pos[:, freeze_index] = 0
+
+            # Adjust positions
+            pos += delta_pos
+
+            # Cool temperature
+            t -= dt
+        return pos
