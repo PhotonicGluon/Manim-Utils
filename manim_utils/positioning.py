a14d0fe9377ff2f5c8790c31b9400991de9f62b8
PhotonicGluon <25820201+PhotonicGluon@users.noreply.github.com>
2024-12-10 13:54:56 +0800

    Add `handle_orientation` toggle to `flip_about_line()`

--- manim_utils/positioning.py
+++ manim_utils/positioning.py
@@ -3,13 +3,15 @@ from manim.mobject.geometry.line import Line
 from manim.mobject.mobject import Mobject
 
 
-def flip_about_line(mobject: Mobject, line: Line) -> None:
+def flip_about_line(mobject: Mobject, line: Line, handle_orientation: bool = True) -> None:
     """
     Flip a mobject about a given line.
 
     Args:
         mobject: The mobject to be flipped.
         line: The line about which to flip the mobject.
+        handle_orientation: Whether to modify the orientation and reflection of the object after
+            flipping. Defaults to True.
     """
 
     # Shift the mobject
@@ -18,6 +20,7 @@ def flip_about_line(mobject: Mobject, line: Line) -> None:
     mobject.shift(2 * (foot_of_perpendicular - center))
 
     # Handle orientation of the mobject
-    mobject.flip()  # To correctly reflect any chiral stuff
-    angle = line.get_angle() - 90 * DEGREES  # Our reference is the vertical, which is 90 degrees
-    mobject.rotate(2 * angle)
+    if handle_orientation:
+        mobject.flip()  # To correctly reflect any chiral stuff
+        angle = line.get_angle() - 90 * DEGREES  # Our reference is the vertical, which is 90 degrees
+        mobject.rotate(2 * angle)
